408af4db8bb0eae777038ccfb45a3f5505b40edd
Victor  Cheng <victorcheng@ResNet-48-126.resnet.ucsb.edu>
2018-01-20 12:04:20 -0800

    ejs working with server

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.post("/", (req, res) => {
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 
+app.get("/", (req, res) => {
+  res.render('index');
 });
 
 app.get("/getchords", (req, res) => {
-
+  
 });
 
 app.listen(process.env.PORT || 5000, () => console.log("Server started"));
